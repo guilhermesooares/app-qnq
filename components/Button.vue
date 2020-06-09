64cc3ae375e82c92cc8e4c992d21979120d403f7
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-06-09 00:39:15 -0300

    Add Button

---
+++ components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="$emit('click')" class="btn btn-1">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.btn {
+  background: none;
+  cursor: pointer;
+  padding: 0.7rem 2rem;
+  display: inline-block;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 700;
+  outline: none;
+  position: relative;
+  transition: all 0.3s;
+}
+.btn-1 {
+  border: 3px solid #fff;
+  color: #fff;
+}
+.btn-1:hover,
+.btn-1:active {
+  color: black;
+  background: #fff;
+}
+</style>
