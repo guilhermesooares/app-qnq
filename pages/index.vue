fd8be1f5953c7bbb4a2765810f19af890b037ca9
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-06-09 00:39:52 -0300

    Add index

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,63 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        app-qnq
-      </h1>
-      <h2 class="subtitle">
-        qnq website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container__header">
+      <Logo />
+    </div>
+    <div class="container__content">
+      <h1>Obrigado por assistir a live do Quarta no Quarto</h1>
+      <p>
+        Faça agora sua doação para o grupo
+        <strong>Colecionando Sorrisos e nos ajude a ajudar</strong>!
+      </p>
+    </div>
+    <div class="container__actions">
+      <Button @click="donate = true">Doar agora</Button>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Button from '~/components/Button'
+import Logo from '~/components/Logo'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Button
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url(styles/styles.scss);
+@import url(styles/normalize.scss);
+@import url(styles/breakpoints.scss);
+
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 1.5rem;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.container {
+  padding: 1.5rem;
+  &__header {
+    svg {
+      width: 6rem;
+    }
+  }
+  &__content {
+    padding-top: 25vh;
+    text-align: center;
+    h1 {
+      font-weight: 700;
+      margin-bottom: 3rem;
+    }
+  }
+  &__actions {
+    .btn {
+      width: 100%;
+    }
+    margin-top: 6rem;
+  }
 }
 </style>
+
